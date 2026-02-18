d3f1dad2a6686cf54269ae6185f3587312b93132
JaiEnfer <prakashjai990@gmail.com>
2026-02-18 20:14:35 +0100

    Minor changes

--- pdf.py
+++ pdf.py
@@ -3,7 +3,7 @@ from pypdf import PdfReader, PdfWriter
 pdf1_path = "main_file.pdf"  # base PDF
 pdf2_path = "page_needed_to_added.pdf"  # to insert at page 2
 pdf3_path = "next_page_needed_to_be_added.pdf"  # to insert at page 3
-output_path = "Master_Thesis_Akshay_Final.pdf"
+output_path = "Final.pdf"
 
 reader1 = PdfReader(pdf1_path)
 reader2 = PdfReader(pdf2_path)
